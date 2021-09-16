218bbd9b5017aa78c935f30543d58c311e846efd
ChrisDan18 <82722850+ChrisDan18@users.noreply.github.com>
2021-09-15 23:51:23 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,9 @@
+function AbrirSeccion2(){
+    $("#seccion1").toggle();
+    $("#seccion2").toggle('slow');
+}
+
+function AbrirSeccion1(){
+    $("#seccion1").toggle('slow');
+    $("#seccion2").toggle();
+}
